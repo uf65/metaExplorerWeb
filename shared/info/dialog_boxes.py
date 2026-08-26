62bb1389a65b74ae1374ed3e2f9becb3e99e22f7
Ulrich Fastenrath <ulrich.fastenrath@gmail.com>
2026-08-26 15:28:20 +0200

    v1.16

--- shared/info/dialog_boxes.py
+++ shared/info/dialog_boxes.py
@@ -4,7 +4,7 @@ import subprocess
 from dotenv import load_dotenv
 import streamlit as st
 
-VERSION = "1.15 Beta"
+VERSION = "1.16 Beta"
 CONTACT_EMAIL = "[metia.explorer@gmx.net](mailto:metia.explorer@gmx.net)"
 
 
@@ -37,7 +37,7 @@ def show_about_dialog():
      
     st.divider()
     st.markdown(f"""
-    **Version {VERSION}** | Stand: Juli 2026
+    **Version {VERSION}** | Stand: August 2026
     """)
     if st.button("OK", use_container_width=True):
         st.rerun()
